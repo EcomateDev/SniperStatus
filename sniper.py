c1fd82516b2ea0b527d70557feac870abd33d444
EcomateDev <144458220+EcomateDev@users.noreply.github.com>
2023-11-14 09:48:34 +0100

    Update sniper.py

--- sniper.py
+++ sniper.py
@@ -1,6 +1,6 @@
 import disnake
 from disnake.ext import commands
-import time
+import asyncio
 from webserver import keep_alive
 
 TOKEN = 'token-here ><'
@@ -8,27 +8,26 @@ TOKEN = 'token-here ><'
 bot = commands.Bot(command_prefix='/', intents=disnake.Intents.default())
 
 async def change_status():
-        await bot.change_presence(activity=disnake.Game(name='[+] SNIPERSTATUS 🛠️'))
-        time.sleep(10)
-        while True:
-          await bot.change_presence(activity=disnake.Game(name=' '))
-          time.sleep(0.3)
-          await bot.change_presence(activity=disnake.Game(name='  '))
-          time.sleep(0.3)
-          await bot.change_presence(activity=disnake.Game(name='   '))
+    await bot.change_presence(activity=disnake.Game(name='[+] SNIPERSTATUS 🛠️'))
+    await asyncio.sleep(3)
+    while True:
+        for _ in range(10):
+            await bot.change_presence(activity=disnake.Game(name=' '))
+            await asyncio.sleep(2)
+            await bot.change_presence(activity=disnake.Game(name='  '))
+            await asyncio.sleep(2)
+            await bot.change_presence(activity=disnake.Game(name='   '))
+            await asyncio.sleep(2)
 
 @bot.event
 async def on_ready():
     bot.loop.create_task(change_status())
-    print('[+] ty for using sniperstatus ^^')
-    time.sleep(0.2)
+    print('[+] Thank you for using SniperStatus!')
     print(f'[/] {bot.user.name} has connected to Discord!')
-    time.sleep(0.7)
     print('[=] SniperStatus is working =)')
-    time.sleep(0.7)
-    print('[!] WARNING: SNIPERSTATUS WATERMARK IS GETTING FOR 10 SECONDS')
-    time.sleep(0.4)
+    print('[!] WARNING: SNIPERSTATUS WATERMARK IS APPEARING FOR 3 SECONDS')
     print('[//] https://github.com/EcomateDev/SniperStatus')
+    print('[//] https://www.patreon.com/EcomateDev')
 
 keep_alive()
 bot.run(TOKEN)
